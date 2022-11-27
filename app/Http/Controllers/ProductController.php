8fb36a68b6f554d2e5eb5666f2c7fdaa4ba78c41
Danilo Oliveira <Daniloworkdev@gmail.com>
2022-11-27 16:55:48 +0000

    feat(API): endpoint get all products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function get_all_product(Request $request)
+    {
+        $products = Product::all();
+        return response()->json([
+            'products' => $products
+        ],200);
+    }
+
 }
