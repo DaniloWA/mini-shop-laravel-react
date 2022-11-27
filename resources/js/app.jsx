a418a9e104e5007685b188060d0c0c4a1e29c67d
Danilo Oliveira <Daniloworkdev@gmail.com>
2022-11-27 17:03:50 +0000

    feat: created and config render react with logic sweetalert2

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,10 +1,29 @@
 import './bootstrap';
 
 import React from 'react';
-import { ReactDOM } from 'react-dom/client';
+import ReactDOM from "react-dom/client"
+import { BrowserRouter } from 'react-router-dom';
 
-import App from './components/app';
+import App from './components/App';
+
+//sweetalert2
+import Swal from 'sweetalert2'
+import 'sweetalert2/dist/sweetalert2.css'
+
+window.Swal = Swal
+
+const toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+})
+window.toast = toast
+
 
 ReactDOM.createRoot(document.getElementById('app')).render(
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
 )
