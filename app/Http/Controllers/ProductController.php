a87a5dad2848cbe882d4047fd73159ed3ac171ff
Danilo Oliveira <Daniloworkdev@gmail.com>
2022-11-27 16:57:22 +0000

    feat(API): endpoint add products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,4 +14,26 @@ class ProductController extends Controller
         ],200);
     }
 
+    public function add_product(Request $request) {
+        $product = new Product();
+
+        $product->name = $request->name;
+        $product->description = $request->description;
+
+        if (!empty($request->photo) && $request->hasFile('photo') ) {
+            $file = $request->file('photo');
+            $extension = $file->getClientOriginalExtension();
+            $filename = time().'.' . $extension;
+            $file->move(public_path('uploads/'), $filename);
+            $product->photo = 'public/uploads/'.$filename;
+        } else {
+             $product->photo = "0";
+        };
+
+        $product->type = $request->type;
+        $product->quantity = $request->quantity;
+        $product->price = $request->price;
+        $product->save();
+
+    }
 }
